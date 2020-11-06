65705377f6e3c3ee931119357677dd1bf340a5ca
rickmort <rickmortdecred@gmail.com>
2020-11-06 13:25:48 -0600

    add decred mainnet comms

--- src/js/data/methods/bitcoin/common.js
+++ src/js/data/methods/bitcoin/common.js
@@ -13,6 +13,7 @@ export const select = [
     { value: 'vtc', label: 'Vertcoin', affectedValue: `m/49'/28'/0'` },
     { value: 'cpc', label: 'Capricoin', affectedValue: `m/44'/289'/0'` },
     { value: 'kmd', label: 'Komodo', affectedValue: `m/44'/289'/0'` },
+    { value: 'dcr', label: 'Decred', affectedValue: `m/44'/42'/0'`}
 ];
 
 export const xpubs = [
